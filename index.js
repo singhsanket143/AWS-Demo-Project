a7f2093109bb1f904c0c50b4bac29f60170e4ccf
Sanket Singh <singhsanket143@gmail.com>
2024-06-23 20:27:24 +0530

    Added changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 
 app.get('/ping', (req, res) => {
+    for(let i = 0; i < 10000000000; i++) {}
     return res.json({
         message: 'pong',
         serverAddress: ip.address()
