b990acec9206b336b88f3069f872234b48d4f625
Sanket Singh <singhsanket143@gmail.com>
2024-06-23 20:37:26 +0530

    Added changes

--- index.js
+++ index.js
@@ -5,13 +5,23 @@ const app = express();
 
 
 app.get('/ping', (req, res) => {
-    for(let i = 0; i < 10000000000; i++) {}
+    
     return res.json({
         message: 'pong',
         serverAddress: ip.address()
     })
 });
 
+
+app.get('/heavy', (req, res) => {
+    console.log("looping")
+    for(let i = 0; i < 10000000000; i++) {}
+
+    return res.json({
+        message: 'heavy',
+        serverAddress: ip.address()
+    });
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
